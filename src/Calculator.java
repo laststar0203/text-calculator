26c03ce96bbfc33b6f85b1da45e60ef8bab3f499
Laststar020203 <44064404+Laststar020203@users.noreply.github.com>
2019-01-24 21:10:42 +0900

    2019/01/24
    -소괄호 , 사칙 연산 완료
    -중괄호  대괄호에 대한 연산만 남음

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,19 +5,22 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
+import java.util.Vector;
 
 public class Calculator {
 
-	enum Operator {
-		RESULT(0 , '='), SBRACKET(1, '['), BRACE(2, '{'),
-		PARENTHESIS(3, '(') , ADD(4, '+'), MINUS(5, '-'), DIVIDE(6, '/'), MULTIPLY(7, '*'); 
+	public static void main(String[] args) {
+		Calculator c = new Calculator();
+		int value = c.calculation("{12+2-(4+5)}+2");
+		System.out.println(value);
+	}
 
-		
+	enum Operator {
+		SBRACKET(1, '['), BRACE(2, '{'), PARENTHESIS(3, '('), ADD(4, '+'), MINUS(5, '-'), DIVIDE(6, '/'),
+		MULTIPLY(7, '*');
+
 		private final int priority;
 		private final char operator;
-		
-		final static int[] arr = {'=' , '+', '-', '/', '*', '[', '{', '(' };
-		
 
 		Operator(int priority, char operator) {
 			this.priority = priority;
@@ -31,7 +34,18 @@ public class Calculator {
 		public char getOperator() {
 			return operator;
 		}
-		
+
+		public static char getOppositionBracket(char text) {
+			switch (text) {
+			case '[':
+				return ']';
+			case '{':
+				return '}';
+			case '(':
+				return ')';
+			}
+			return (Character) null;
+		}
 
 		public static Operator valueOf(char c) {
 			switch (c) {
@@ -49,82 +63,138 @@ public class Calculator {
 				return Operator.BRACE;
 			case '(':
 				return Operator.PARENTHESIS;
-			case '=':
-				return Operator.RESULT;
 			}
 			return null;
 		}
 
 		public static boolean isOperator(int text) {
 			boolean isOperator = false;
-			for (int i : arr)
-				if (i == text)
+			for (Operator i : Operator.values())
+				if ((int) i.operator == text)
 					isOperator = true;
 			return isOperator;
 		}
-		
+
 	}
 
-	String string;
-	Stack<String> st_expression;
-	Stack<Operator> st_operator;
-
 	public Calculator() {
-		this.st_expression = new Stack<>();
-		this.st_operator = new Stack<>();
+
 	}
 
 	public int calculation(String expression) {
+		int result;
 
-		makePostfixNotation(expression);
-		System.out.println(st_expression);
-		System.out.println(st_operator);
-		
+		try {
+			result = interpretExperssion(makePostfixNotation(expression));
+		} catch (Exception e) {
+			e.printStackTrace();
+			result = 0;
+		}
+
+		return result;
+
+	}
+
+	private String makePostfixNotation(String expression) throws Exception {
+		StringBuilder numberSb = new StringBuilder();
+		StringBuilder expSb = new StringBuilder();
+
+		Stack<Operator> st_operator = new Stack<>();
+
+		char inBracket = 0;
+
+		for (int i = 0; i < expression.length(); i++) {
+			char c = expression.charAt(i);
+
+			if (c >= '0' && c <= '9' || c == '.')
+				numberSb.append(c);
+			else if (Operator.isOperator(c)) {
+
+				Operator op = Operator.valueOf(c);
+				if (op.priority < 4) {
+
+					inBracket = op.getOperator();
+					st_operator.push(op);
+				} else {
+					expSb.append(numberSb.toString() + " ");
+
+					numberSb.delete(0, numberSb.length());
+					if (!st_operator.isEmpty() && st_operator.peek().priority >= op.priority) {
+						expSb.append(Character.toString(st_operator.pop().getOperator()) + " ");
+						st_operator.push(op);
+					} else
+						st_operator.push(op);
+				}
+			} else if (inBracket != 0 && c == Operator.getOppositionBracket(inBracket)) {
+				expSb.append(numberSb.toString() + " ");
+				numberSb.delete(0, numberSb.length());
+				while (st_operator.peek().getOperator() != inBracket) {
+
+					expSb.append(Character.toString(st_operator.pop().getOperator()) + " ");
+				}
+				inBracket = 0;
+				st_operator.pop(); // exit bracket
+			} else
+				throw new Exception("Invalid character found. count :" + i);
+
+		}
+		while (!st_operator.isEmpty())
+			expSb.append(Character.toString(st_operator.pop().getOperator()) + " ");
+
+		return expSb.toString();
+	}
+
+	private int interpretExperssion(String expression) throws Exception {
+
+		Stack<Integer> st_value = new Stack<>();
+		Stack<String> st_expression = new Stack<>();
+
+		String[] textToken = expression.split(" ");
+
+		for (int i = textToken.length - 1; i >= 0; i--)
+			st_expression.push(textToken[i]);
+
+		while (!st_expression.isEmpty()) {
+			String token = st_expression.pop();
+
+			try {
+				int number = Integer.parseInt(token);
+				st_value.push(number);
+			} catch (NumberFormatException nfe) {
+
+				char operator = token.toCharArray()[0];
+
+				int secondNum = st_value.pop();
+				int firstNum = st_value.pop();
+
+				st_value.push(operationCalculation(firstNum, operator, secondNum));
+
+			}
+
+		}
+
+		int result = st_value.pop();
+		if (!st_value.isEmpty())
+			throw new Exception("Invalid expression");
+
+		return result;
+
+	}
+
+	private int operationCalculation(int firstNum, char operator, int secondNum) {
+
+		switch (operator) {
+		case '+':
+			return firstNum + secondNum;
+		case '-':
+			return firstNum - secondNum;
+		case '*':
+			return firstNum * secondNum;
+		case '/':
+			return firstNum / secondNum;
+		}
+
 		return 0;
 	}
 
-	private void makePostfixNotation(String expression) {
-
-		StringBuilder sb = new StringBuilder();
-		boolean inBracket = false;
-		
-		try {
-			for (int i = 0; i < expression.length(); i++) {
-				char c = expression.charAt(i);
-
-				if (c >= '0' && c <= '9' || c == '.')
-					sb.append(c);
-				else if (Operator.isOperator(c)) {
-					st_expression.push(sb.toString());
-					sb.delete(0, sb.length());
-							
-					Operator op = Operator.valueOf(c);
-					if(op.getPriority() > 2) { inBracket = true;
-						continue;
-					}
-					if(inBracket) {
-						st_operator.push(op);
-						inBracket = false;
-					}
-						
-					
-					if (!st_operator.isEmpty() && st_operator.peek().getPriority() >= op.getPriority()) {
-						st_expression.push(st_operator.pop().getOperator()+"");
-					}				
-					st_operator.push(op);
-					System.out.println(op + " : "+ i);
-				}else
-					throw new Exception("Invalid character found.");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-				
-		}
-
-	public static void main(String[] args) {
-		Calculator c = new Calculator();
-		c.calculation("12+2+(4+5=");
-	}
-	
 }
